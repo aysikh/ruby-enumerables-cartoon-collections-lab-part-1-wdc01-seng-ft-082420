c491b3c2c1222a1941c2293b707a768ff9e63199
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-17 05:28:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index(1) do |dwarves| 
-  print dwarves
+  array.each_with_index(1) do |list_dwarves| 
+  print list_dwarves
 end
 end
